2d6b02a2f0a2ac3faae34b7ee761c26770041fd3
Enzo <enzohakim2008@gmail.com>
2026-03-27 09:36:26 +0200

    Shaping the mess into a TUI app. TODO, make this compile, add the OS library to make it actually work

--- src/build/_deps/ftxui-build/CMakeFiles/dom.dir/src/ftxui/dom/flexbox_config.cpp.o.d
+++
@@ -1,4 +0,0 @@
-_deps/ftxui-build/CMakeFiles/dom.dir/src/ftxui/dom/flexbox_config.cpp.o: \
- /home/enzo/Documents/Code/AuTUI/src/build/_deps/ftxui-src/src/ftxui/dom/flexbox_config.cpp \
- /usr/include/stdc-predef.h \
- /home/enzo/Documents/Code/AuTUI/src/build/_deps/ftxui-src/include/ftxui/dom/flexbox_config.hpp
--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,57 @@
-#include <ftxui/dom/elements.hpp>
-#include <ftxui/screen/screen.hpp>
-#include <iostream>
- 
+// Copyright 2020 Arthur Sonzogni. All rights reserved.
+// Use of this source code is governed by the MIT license that can be found in
+// the LICENSE file.
+#include <iostream> // for basic_ostream::operator<<, operator<<, endl, basic_ostream, basic_ostream<>::__ostream_type, cout, ostream
+#include <memory>   // for shared_ptr, __shared_ptr_access
+#include <string>   // for to_string, allocator
+// #include "ftxui/component/app.hpp"             // for App
+#include "ftxui/component/captured_mouse.hpp" // for ftxui
+#include "ftxui/component/component.hpp" // for MenuEntryAnimated, Renderer, Vertical
+#include "ftxui/component/component_base.hpp"    // for ComponentBase
+#include "ftxui/component/component_options.hpp" // for MenuEntryAnimated
+#include "ftxui/dom/elements.hpp" // for operator|, separator, Element, Decorator, color, text, hbox, size, bold, frame, inverted, vbox, HEIGHT, LESS_THAN, border
+#include "ftxui/screen/color.hpp" // for Color, Color::Blue, Color::Cyan, Color::Green, Color::Red, Color::Yellow
+
+using namespace ftxui;
+
+// Define a special style for some menu entry.
+MenuEntryOption Colored(ftxui::Color c) {
+  MenuEntryOption option;
+  option.animated_colors.foreground.enabled = true;
+  option.animated_colors.background.enabled = true;
+  option.animated_colors.background.active = c;
+  option.animated_colors.background.inactive = Color::Black;
+  option.animated_colors.foreground.active = Color::White;
+  option.animated_colors.foreground.inactive = c;
+  return option;
+}
+
 int main() {
-  using namespace ftxui;
- 
-  // Create a simple document with three text elements.
-  Element document = hbox({
-    text("left")   | border,
-    text("middle") | border | flex,
-    text("right")  | border,
-  });
- 
-  // Create a screen with full width and height fitting the document.
-  auto screen = Screen::Create(
-    Dimension::Full(),       // Width
-    Dimension::Fit(document) // Height
+  auto screen = ftxui::Screen::Create(ftxui::Dimension::Full(),   // width
+                                      ftxui::Dimension::Fixed(10) // height
   );
- 
-  // Render the document onto the screen.
-  Render(screen, document);
- 
-  // Print the screen to the console.
-  screen.Print();
+  int selected = 0;
+  auto menu = Container::Vertical(
+      {
+          MenuEntry(" 1. CD", Colored(Color::Red)),
+          MenuEntry(" 2. mkdir", Colored(Color::Yellow)),
+          MenuEntry(" 3. pacman -Syu", Colored(Color::Green)),
+          MenuEntry(" 4. rm", Colored(Color::Cyan)),
+          MenuEntry(" 5. quit", Colored(Color::Blue)),
+      },
+      &selected);
+
+  // Display together the menu with a border
+  auto renderer = Renderer(menu, [&] {
+    return vbox({
+               hbox(text("selected = "), text(std::to_string(selected))),
+               separator(),
+               menu->Render() | frame,
+           }) |
+           border | bgcolor(Color::Black);
+  });
+
+  screen.Loop(renderer);
+
+  std::cout << "Selected element = " << selected << std::endl;
 }
