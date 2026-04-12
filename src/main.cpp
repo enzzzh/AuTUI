604323d3902847511c90bba96ef52aa53f810b32
Enzo <enzohakim2008@gmail.com>
2026-04-12 15:54:15 +0300

    fix: add TUI loop that actually works, chdir logic and pretty good input handling

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,3 @@
-// Copyright 2020 Arthur Sonzogni. All rights reserved.
-// Use of this source code is governed by the MIT license that can be found in
-// the LICENSE file.
 #include "ftxui/component/component.hpp"
 #include "ftxui/component/component_base.hpp"
 #include "ftxui/component/component_options.hpp"
@@ -8,128 +5,147 @@
 #include "ftxui/component/screen_interactive.hpp"
 #include "ftxui/dom/elements.hpp"
 #include "ftxui/screen/color.hpp"
+#include <cerrno>
+#include <climits>
+#include <cstddef>
+#include <cstring>
 #include <ftxui/component/component.hpp>
 #include <iostream>
+#include <limits>
 #include <ostream>
 #include <string>
+#include <unistd.h>
 
 using namespace ftxui;
+using namespace std;
 
 MenuEntryOption Colored(ftxui::Color c) {
-    MenuEntryOption option;
-    option.animated_colors.foreground.enabled = true;
-    option.animated_colors.background.enabled = true;
-    option.animated_colors.background.active = c;
-    option.animated_colors.background.inactive = Color::Black;
-    option.animated_colors.foreground.active = Color::White;
-    option.animated_colors.foreground.inactive = c;
-    return option;
+  MenuEntryOption option;
+  option.animated_colors.foreground.enabled = true;
+  option.animated_colors.background.enabled = true;
+  option.animated_colors.background.active = c;
+  option.animated_colors.background.inactive = Color::Black;
+  option.animated_colors.foreground.active = Color::White;
+  option.animated_colors.foreground.inactive = c;
+  return option;
 }
 
 int main() {
 
-    std::string password;
-    std::cout
-        << "What is the password for your user ? (note this is stored locally)"
-        << std::endl;
-    std::cin >> password;
-
+  string password;
+  cout << "What is the password for your user ? (note this is stored locally)"
+       << endl;
+  getline(cin, password);
+  bool quit = false;
+  while (!quit) {
     auto screen2 = ScreenInteractive::TerminalOutput();
     auto screen = ScreenInteractive::TerminalOutput();
     int selected = 0;
     auto menu = Container::Vertical(
-            {
+        {
             MenuEntry(" 1. CD", Colored(Color::Red)),
             MenuEntry(" 2. mkdir", Colored(Color::Yellow)),
             MenuEntry(" 3. Install package", Colored(Color::Green)),
             MenuEntry(" 4. rm", Colored(Color::Cyan)),
             MenuEntry(" 5. Other commands (work in progress )",
-                    Colored(Color::Cyan)),
+                      Colored(Color::Cyan)),
             MenuEntry(" 6. quit", Colored(Color::Blue)),
-            },
-            &selected);
+        },
+        &selected);
 
     // Display together the menu with a border
     auto renderer = Renderer(menu, [&] {
-            return vbox({
-                    hbox(text("selected = "), text(std::to_string(selected))),
-                    separator(),
-                    menu->Render() | frame,
-                    }) |
-            border | bgcolor(Color::Black);
-            });
+      return vbox({
+                 hbox(text("selected = "), text(to_string(selected))),
+                 separator(),
+                 menu->Render() | frame,
+             }) |
+             border | bgcolor(Color::Black);
+    });
 
     // This is the only thing I need for debugging
-    std::cout << "Selected element = " << selected << std::endl;
+    cout << "Selected element = " << selected << std::endl;
 
     auto component = CatchEvent(renderer, [&](Event event) {
-            if (event == Event::Return) {
-            screen.ExitLoopClosure()();
-            return true;
-            }
-            return false;
-            });
+      if (event == Event::Return) {
+        screen.ExitLoopClosure()();
+        return true;
+      }
+      return false;
+    });
     screen.Loop(component);
     try {
-        switch (selected) {
-            case 0: {
-                        std::string dir;
-                        std::cout << "Enter directory name to create: ";
-                        std::cin >> dir;
-                        std::string cmd = "cd " + dir;
-                        if (system(cmd.c_str()) != 0)
-                            throw std::runtime_error("cd failed.");
-                        break;
-                    }
-            case 1: {
-                        std::string dir;
-                        std::cout << "Enter directory name to create: ";
-                        std::cin >> dir;
-                        std::string cmd = "mkdir " + dir;
-                        if (system(cmd.c_str()) != 0)
-                            throw std::runtime_error("mkdir failed.");
-                        break;
-                    }
-            case 2: {
-                        std::string package;
-                        std::cout << "Enter the package name you want to install ";
-                        std::cin >> package;
-                        // Note the space after the password and the -S flag
-                        std::string cmd = "echo " + password + " | sudo -S pacman -S " + package +
-                            " --noconfirm";
-                        system(cmd.c_str());
-                        // std::string cmd = "sudo pacman -S " + package + " --noconfirm";
-                        if (system(cmd.c_str()) != 0) {
-                            throw std::runtime_error("pacman failed");
-                        }
-                        break;
-                    }
-            case 3: {
-                        std::string directory;
-                        std::cout << "Enter the directory you want to remove ?";
-                        std::cin >> directory;
-                        std::string cmd = "sudo rm -fr " + directory;
-                        if (system(cmd.c_str()) != 0) {
-                            throw std::runtime_error("rm failed !");
-                        }
-                        break;
-                    }
-            case 4: {
-                        std::string command;
-                        std::cout << "Enter the command you want to input ";
-                        std::cin >> command;
-                        std::string cmd = command;
-                        if (system(cmd.c_str()) != 0) {
-                            throw std::runtime_error("Command run failed");
-                        }
-                        break;
-                    }
-            case 5: {
-                        break;
-                        return 0;
-                    }
+      switch (selected) {
+      case 0: {
+        string dir;
+        cout << "Enter name of the directory you want to move to: ";
+        getline(cin, dir);
+        // unused, if chdir don't works
+        // string cmd = "cd " + dir;
+
+        if (chdir(dir.c_str()) != 0) {
+          throw runtime_error("cd failed " + string(strerror(errno)));
         }
-    } catch (const std::exception &e) {
-        std::cerr << "[Error] " << e.what() << std::endl;
+
+        char cwd[PATH_MAX];
+        if (getcwd(cwd, sizeof(cwd)) != NULL) {
+          cout << "It worked, current directory: " << cwd << endl;
+        }
+        break;
+      }
+      case 1: {
+        string dir;
+        cout << "Enter directory name to create: ";
+
+        getline(cin, dir);
+        string cmd = "mkdir " + dir;
+        if (system(cmd.c_str()) != 0)
+          throw runtime_error("mkdir failed.");
+        break;
+      }
+      case 2: {
+        string package;
+        cout << "Enter the package name you want to install ";
+
+        getline(cin, package);
+        string cmd = "echo " + password + " | sudo -S pacman -S " + package +
+                     " --noconfirm";
+        // system(cmd.c_str());
+        if (system(cmd.c_str()) != 0) {
+          throw runtime_error("pacman failed");
+        }
+        break;
+      }
+      case 3: {
+        string directory;
+        cout << "Enter the directory you want to remove ?";
+
+        getline(cin, directory);
+        string cmd = "sudo rm -fr " + directory;
+        if (system(cmd.c_str()) != 0) {
+          throw runtime_error("rm failed !");
+          cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        }
+        break;
+      }
+      case 4: {
+        string command;
+        cout << "Enter the command you want to input ";
+
+        getline(cin, command);
+        string cmd = command;
+        if (system(cmd.c_str()) != 0) {
+          throw runtime_error("Command run failed");
+        }
+        break;
+      }
+      case 5: {
+        break;
+        return 0;
+      }
+      }
+    } catch (const exception &e) {
+      cerr << "[Error] " << e.what() << std::endl;
     }
+  }
 }
